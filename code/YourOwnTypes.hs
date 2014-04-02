42a622bf609e5582f18c9b91c82a393786b9b1dc
Jeff Foster <jeff.foster@acm.org>
2014-04-02 07:41:33 +0100

    add more example code

---
+++ code/YourOwnTypes.hs
@@ -0,0 +1,17 @@
+module YourOwnTypes where
+
+-- Game is the type
+-- GameC is the type signature
+data Game = GameC String Int String Int
+            
+-- Function type signatures use the type
+result :: Game -> String
+
+-- Function definitions "Deconstruct" values with the type constructor
+result (GameC t1 s1 t2 s2)
+  | s1 > s2   = t1 ++ " wins"
+  | s2 > s1   = t2 ++ " wins"
+  | otherwise = "Draw"
+
+example :: Game
+example = GameC "Everton" 4 "Manchester United" 0
