bc87d40726d2499fb843f487577f9e02901984c2
Jeff Foster <jeff.foster@acm.org>
2014-04-04 18:04:06 +0100

    basic league table comparing half-time results

---
+++ data/Example.hs
@@ -0,0 +1,41 @@
+module Example where
+
+import FootballStats
+
+import Data.List
+import Data.Ord
+
+-- Team and points
+type LeagueTable = [(String,Int)]
+
+leagueTable :: [FootballResult] -> LeagueTable
+leagueTable xs = foldl' updateLeagueTable initialTable xs
+  where
+    initialTable = zip (nub (map homeTeam xs)) [0..]
+
+update :: String -> Int -> LeagueTable -> LeagueTable
+update team delta []     = []
+update team delta (x:xs)
+  | fst x == team = (fst x, snd x + delta) : update team delta xs
+  | otherwise = x : update team delta xs
+
+updateLeagueTable :: LeagueTable -> FootballResult -> LeagueTable
+updateLeagueTable lt fr = update at atp $ update ht htp lt
+  where
+    ht = homeTeam fr
+    at = awayTeam fr
+    htp = homeTeamPoints fr
+    atp = awayTeamPoints fr
+
+homeTeamPoints :: FootballResult -> Int
+homeTeamPoints fr
+  | halfTimeHomeGoals fr >  halfTimeAwayGoals fr = 3
+  | halfTimeHomeGoals fr == halfTimeAwayGoals fr = 1
+  | otherwise                                    = 0
+
+awayTeamPoints fr
+  | x == 3  = 0
+  | x == 1  = 1
+  | x == 0  = 3
+  where
+    x = homeTeamPoints fr
