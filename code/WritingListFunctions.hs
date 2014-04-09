0028d40b9e2099292b5c62b7c0abb08ef5bd1945
Jeff Foster <jeff.foster@acm.org>
2014-04-09 21:55:05 +0100

    add example for writing list functions

---
+++ code/WritingListFunctions.hs
@@ -0,0 +1,41 @@
+module WritingListFunctions where
+
+import Test.QuickCheck
+import Text.Printf
+
+myContains :: Eq a => a -> [a] -> Bool
+myContains e []     = False
+myContains e (x:xs)
+  | e == x          = True
+  | otherwise       = myContains e xs
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter fn []     = []
+myFilter fn (x:xs)
+  | fn x           = x : myFilter fn xs
+  | otherwise      = myFilter fn xs
+
+myReverseNaive :: [a] -> [a] -- (suprisingly hard!)
+myReverseNaive [] = []
+myReverseNaive (x:xs) = myReverseNaive xs ++ [x]
+
+myReverse :: [a] -> [a]
+myReverse xs = myReverse' xs []
+  where
+    myReverse' []     ys = ys
+    myReverse' (x:xs) ys = myReverse' xs (x:ys)
+
+-- I can test whether these are correct using QuickCheck
+-- A property based testing framework that asserts invariants
+prop_oracle_reverse :: Eq a => [a] -> Bool
+prop_oracle_reverse xs = myReverse xs == reverse xs
+
+prop_reverse_preserves_length :: [a] -> Bool
+prop_reverse_preserves_length xs = length (myReverse xs) == length xs
+
+-- Run tests with
+-- quickCheck prop_oracle_reverse
+-- quickCheck prop_reverse_preserves_length
+-- See more with verboseCheck instead of quickCheck
+
+-- See what happens when it fails by removing `x` in the myReverse definition
